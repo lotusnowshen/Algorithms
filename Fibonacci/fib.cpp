33ebf1434971db3ba36404acab423c0d97be04fb
haha <guochy2012@gmail.com>
2014-03-16 00:16:22 +0800

    Add Fibonacci

--- Fibonacci/fib.cpp
+++ Fibonacci/fib.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include <string>
 
+long long fibonacci(std::size_t n);
+
 int main()
 {
-    
+    std::cout << fibonacci(4) << std::endl;
+    std::cout << fibonacci(5) << std::endl;
 
 }
 
-long fib(long n)
+
+long long fibonacci(std::size_t n)
 {
     if (n == 0)
         return 0;
-    else if (n == 1)
+    if (n == 1)
         return 1;
-    else 
-        return fib(n-1) + fib(n-2);
-}
+    long long fib_result = 0;
+    long long fib_min_one = 1;
+    long long fib_min_two = 0;
 
-long fibonacci(long n)
-{
-
-    return n;
+    for(std::size_t ix = 2; ix <= n; ++ix)
+    {
+        fib_result = fib_min_one + fib_min_two;
+        fib_min_two = fib_min_one;
+        fib_min_one = fib_result;
+    }
+    return fib_result;
 }
 
     
